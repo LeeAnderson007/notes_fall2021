fc5ff3066caeb70fe33177a58cfe83598be9c084
Lee Anderson <ecllips07@gmail.com>
2021-08-24 08:23:21 -0600

    added controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,16 @@
 class NotesController < ApplicationController
   def index
+    notes = Note.all
+    render component: "Notes", props: {notes: notes}
   end
 
   def show
+    note = Note.find(params[:id])
+    render component: "Note", props: {note: note} 
   end
 
   def new
+    note = Note.new
+    render component: "NoteNew", props: {page: page} 
   end
 end
